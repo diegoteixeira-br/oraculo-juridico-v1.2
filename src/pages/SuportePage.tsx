fff687b5633ce8fd4efbd614be35e37a2e7af522
Diego T. da Silva <dieguinhoteixeira@gmail.com>
2025-08-02 12:03:42 -0400

    Updated SuportePage.tsx

--- src/pages/SuportePage.tsx
+++ src/pages/SuportePage.tsx
@@ -14,7 +14,7 @@ const SuportePage = () => {
   
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
-  const whatsappNumber = "5511987654321"; // Substitua pelo número real do WhatsApp
+  const whatsappNumber = "5565993025105"; // Substitua pelo número real do WhatsApp
   
   const supportCategories = [
     {
